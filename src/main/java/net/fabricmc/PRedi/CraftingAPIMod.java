5ec60aa59a8fe67fb65a07c11cdfc26d628e6ab1
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2020-09-13 13:49:03 -0400

    Added ChauldronRecipeReader

--- src/main/java/net/fabricmc/PRedi/CraftingAPIMod.java
+++ src/main/java/net/fabricmc/PRedi/CraftingAPIMod.java
@@ -8,9 +8,6 @@ public class CraftingAPIMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-        ModRegistry.initShit();
-
 		System.out.println("Hello Fabric world!");
 	}
 }
