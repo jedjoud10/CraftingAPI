b924985fafe15cb45f8c1b4622db6e533f3b6ab4
PiggyLogan <43256029+PiggyLogan@users.noreply.github.com>
2020-09-13 16:14:33 -0400

    a
    a

--- src/main/java/net/fabricmc/PRedi/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/PRedi/mixin/ExampleMixin.java
@@ -1,12 +1,13 @@
 package net.fabricmc.PRedi.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.entity.mob.AbstractSkeletonEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(AbstractSkeletonEntity.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
